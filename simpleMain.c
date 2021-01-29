91ffc32b005a9b672ddac5a9713a0bba7147289e
ForestGoose <chayawat.pahn1910@gmail.com>
2021-01-29 11:15:18 +0000

    Fixing "simpleMain.c" for appropriate data.

--- simpleMain.c
+++ simpleMain.c
@@ -1,27 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include "bubble.h"
 #include "selection.h"
 #include "insertion.h"
 int main(int argc, char *argv[]){
-
-  int i,N;
-  printf("There are %d arguments\n",argc);
-  for(i=0;i<argc;i++){
-         printf("argv %d: %s\n",i,argv[i]); 
-  }
-
-  N=argc-1;
-
-  int *a;
-  a=(int*) malloc(sizeof(int)*N);
-
-  for(i=1;i<argc;i++){
-        a[i-1]=atoi(argv[i]);
-  }
-
-  //bubble(a,N);
- // selection(a,N);
- insertion(a,N);
+  int i,*a,N = argc-2;
+  char* type = argv[1];
+  a = (int*)malloc(sizeof(int)*N);
+  //printf("argument 0 is %s\n",*argv);
+  for(i=2;i<argc;i++)
+    a[i-2] = atoi(argv[i]);
+  if(strcmp(type,"bubble") == 0) bubble(a,N);
+  else if(strcmp(type,"selection") == 0) selection(a,N);
+  else if(strcmp(type,"insertion") == 0) insertion(a,N);
+  else printf("Error argument\n");
   return 0;
 }
